bedf1bf2faf5e8206a06469a8dd86f58eb24e6a1
Ken Alex Fassone <nexusventuri@gmail.com>
2016-03-10 19:29:08 +0000

    Set up puma

---
+++ config/puma/development.rb
@@ -0,0 +1,13 @@
+workers Integer(ENV['WEB_CONCURRENCY'] || 1)
+threads_count = Integer(ENV['MAX_THREADS'] || 3)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+
+bind 'tcp://0.0.0.0:3002'
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
